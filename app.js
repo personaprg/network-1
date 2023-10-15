1c41c563379d2d6b7d46511e5c022d28a0f95cdd
Garry <personarpg7501@gmail.com>
2023-10-16 04:01:56 +0900

    인코딩 방식(utf8) 추가 <= (utf8 = 한글)

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const server = http.createServer((req, res) => {
   //2. 요청 메서드
 
   if(req.url === "/" && req.method === "GET"){
-    fs.readFile("./index.html")
+    fs.readFile("./index.html", "utf8")
 
   }
 

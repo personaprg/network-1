5fbcf223d0fed4ab2f9fc886d5c7a7e4f7016e7a
Garry <personarpg7501@gmail.com>
2023-10-16 03:49:15 +0900

    서버에서 문제가 발생 할 경우 출력되는 메시지 리턴 설정

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const server = http.createServer((req, res) => {
   function serverErrorLog(){ //서버 오류 로그를 확인하기 위한 func 생성
 
     res.writeHead(500);
+    return res.end("서버의서 문제가 발생하였습니다.");
   }
 
 });

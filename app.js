df3804895e8fcbdb70fcbc3c7b608ac25b08a2cf
Garry <personarpg7501@gmail.com>
2023-10-16 04:10:50 +0900

    CSS 파일 가져오기, 현재 폴도 기준 "./" 에서 시작

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, {"Countent-Type": "text/html"});
       res.end(data);
     });
-  }else if(CSS);
+  }else if(req.url === "./");
 
 });
